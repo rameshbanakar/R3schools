f50fd1e3b14fd101fcca5cd3b000aaa6d8321439
ramesh banakar <ramesh.s.b@campusuvce.in>
2023-09-28 03:56:16 +0530

    hpp helmet and mongo senitizer added

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB=require("./db")
 const cors=require("cors")
+const helmet=require("helmet")
+const xss=require("xss-clean")
+const hpp=require("hpp")
+const mongoSanitize=require("express-mongo-sanitize")
 const authRouter=require("./Router/authRouter")
 const cookieParser = require("cookie-parser");
 dotenv.config({ path: "./config/config.env" });
@@ -13,6 +17,11 @@ app.use(cookieParser());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+app.use(mongoSanitize());
+app.use(helmet());
+//cross site scriptinng
+app.use(xss());
+app.use(hpp());
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.listen(5000, () => console.log("server started@localhost 5000"));
